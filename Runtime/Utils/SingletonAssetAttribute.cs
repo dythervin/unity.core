73d5c0f5ddc96cf5c21ea14231ad387975ef9a0c
Dastan Dzhumabekov <dastan.wm@gmail.com>
2022-08-18 11:59:00 +0600

    Fixes

---
+++ Runtime/Utils/SingletonAssetAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dythervin.Core.Utils
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SingletonAssetAttribute : Attribute
+    {
+        public const string DefaultPath = "Global";
+        public readonly string resourcePath;
+
+        public SingletonAssetAttribute() : this(DefaultPath) { }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="resourcePath">Relative to resources folder</param>
+        public SingletonAssetAttribute(string resourcePath)
+        {
+            this.resourcePath = resourcePath;
+        }
+    }
+}
