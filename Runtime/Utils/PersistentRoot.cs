cb169e2f2d2c7f1f66f2564246a70bca1dee0bff
Dythervin <dythervin@gmail.com>
2022-09-08 18:02:46 +0600

    Persistence root changed

--- Runtime/Utils/PersistentRoot.cs
+++ Runtime/Utils/PersistentRoot.cs
@@ -5,25 +5,9 @@ namespace Dythervin.Core.Utils
 {
     public static class PersistentRoot
     {
-        private static Transform _instance;
-        private static bool _initialized;
         private static readonly Dictionary<string, GameObject> Transforms = new Dictionary<string, GameObject>();
 
-        public static Transform Transform
-        {
-            get
-            {
-                if (!_initialized)
-                {
-                    var gameObject = new GameObject(nameof(PersistentRoot)) { isStatic = true };
-                    Object.DontDestroyOnLoad(gameObject);
-                    _instance = gameObject.transform;
-                    _initialized = true;
-                }
-
-                return _instance;
-            }
-        }
+        public static Transform Transform { get; private set; }
 
         public static GameObject Get(string name)
         {
@@ -31,6 +15,14 @@ namespace Dythervin.Core.Utils
                 Transforms[name] = target = new GameObject(name) { isStatic = true, transform = { parent = Transform } };
 
             return target;
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Init()
+        {
+            var gameObject = new GameObject(nameof(PersistentRoot)) { isStatic = true };
+            Object.DontDestroyOnLoad(gameObject);
+            Transform = gameObject.transform;
         }
     }
 }
