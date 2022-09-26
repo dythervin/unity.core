1e89ffdb5c47aa700f5e3a897ca753a379f132d4
Dastan Dzhumabekov <dythervin@gmail.com>
2022-09-26 22:24:54 +0600

    Added extensions

---
+++ Runtime/Extensions/StringExt.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dythervin.Core.Extensions
+{
+    public static class StringExt
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static char GetLast(this StringBuilder builder)
+        {
+            return builder[builder.Length - 1];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static StringBuilder PopLast(this StringBuilder builder, int count = 1)
+        {
+            return builder.Remove(builder.Length - count, count);
+        }
+    }
+}
